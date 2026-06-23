0f1b72e60f9a9196f62b24d92ec2b0d45c678c90
kevin L <cadl0898@gmail.com>
2026-06-23 12:03:17 -0500

    API SpringBoot sin configurar

---
+++ API_SpringBoot/maskot-api/src/main/java/com/maskot/maskot_api/MaskotApiApplication.java
@@ -0,0 +1,13 @@
+package com.maskot.maskot_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaskotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaskotApiApplication.class, args);
+	}
+
+}
---
+++ API_SpringBoot/maskot-api/src/test/java/com/maskot/maskot_api/MaskotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maskot.maskot_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaskotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
